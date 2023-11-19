7ba2e230d0ad2cbab9efcb1670cc997c82605096
Dr. Ernie Prabhakar <ernest@quiltdata.io>
2023-11-18 21:32:19 -0800

    lint projen

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,16 +1,16 @@
-import { awscdk } from "projen";
+import { awscdk } from 'projen';
 const project = new awscdk.AwsCdkTypeScriptApp({
-  cdkVersion: "2.1.0",
-  defaultReleaseBranch: "main",
-  description: "AWS CDK project for VIVOS",
-  name: "vivos",
+  cdkVersion: '2.1.0',
+  defaultReleaseBranch: 'main',
+  description: 'AWS CDK project for VIVOS',
+  name: 'vivos',
   projenrcTs: true,
   deps: [
-    "aws-lambda",
-    "aws-sdk",
-    "axios",
+    'aws-lambda',
+    'aws-sdk',
+    'axios',
     // 'config',
-    "dotenv",
+    'dotenv',
   ],
   // devDeps: [],             /* Build dependencies for this module. */
 });
