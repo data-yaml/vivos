71eaccf2e485c8d558c68f33cdb5527ff6acc79a
Dr. Ernie Prabhakar <ernest@quiltdata.io>
2024-01-06 22:34:03 -0800

    PipeQuilt constants

--- src/pipe.quilt.ts
+++ src/pipe.quilt.ts
@@ -1,6 +1,16 @@
 import { Pipe } from './pipe';
 
 export class PipeQuilt extends Pipe {
+
+  public static QUILT_DEFAULTS = {
+    QUILT_VPC: 'sales-production',
+    QUILT_DOCKER_IMAGE: '850787717197.dkr.ecr.us-east-1.amazonaws.com/edp-container-registry:latest',
+  };
+
+  public static QUILT_KEYS(): string[] {
+    return Object.keys(PipeQuilt.QUILT_DEFAULTS);
+  }
+
   public static getPrefix(): string {
     return 'quilt';
   }
